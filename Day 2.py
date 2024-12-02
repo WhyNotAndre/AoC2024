ee918aa0d6d92b937d3e1fa4018159d60e92b10f
Andre L. <76952766+WhyNotAndre@users.noreply.github.com>
2024-12-02 14:33:04 +0300

    Update Day 2.py
    cleaner code

--- Day 2.py
+++ Day 2.py
@@ -1,4 +1,4 @@
-fpath = r""
+fpath = r""  # path to input file
 
 with open(fpath, 'r') as f:
     contents = f.read()
@@ -7,10 +7,10 @@ lists_to_test = [[int(num) for num in line.split()] for line in contents.splitli
 
 def check_seq(lst):
     res1 = ""; res2 = ""
-    for i in range(0, len(lst)-1):
-        res1 = True if (lst[i+1] > lst[i] and abs(lst[i+1] - lst[i]) < 4 and (res1 == True or res1 == "") and lst[i+1] != lst[i]) else False
-    for i in range(0, len(lst)-1):
-        res2 = True if (lst[i+1] < lst[i] and abs(lst[i+1] - lst[i]) < 4 and (res2 == True or res2 == "") and lst[i+1] != lst[i]) else False
+    for i in range(len(lst)-1):
+        res1 = (lst[i + 1] > lst[i] and abs(lst[i + 1] - lst[i]) < 4) and (res1 or res1 == "")
+    for i in range(len(lst)-1):
+        res2 = (lst[i + 1] < lst[i] and abs(lst[i + 1] - lst[i]) < 4) and (res2 or res2 == "")
     return (res1, res2)
             
 # PART 1
@@ -24,7 +24,7 @@ print(r)
 # PART 2
 r = 0
 for l in lists_to_test:
-    for i in range(0, len(l)):
+    for i in range(len(l)):
         lst = l[:i]+l[i+1:]
         a, b = check_seq(lst)
         if a or b:
