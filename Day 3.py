c3b1dad60fcf73e2b3b40d9e41d24abf29b814ac
Andre L. <76952766+WhyNotAndre@users.noreply.github.com>
2024-12-03 09:18:06 +0300

    Update Day 3.py
    added part 2

--- Day 3.py
+++ Day 3.py
@@ -1,20 +1,41 @@
 import re
+sum1 = 0; sum2 = 0; state = False
 
 fpath = r"" # path to input file
-
 with open(fpath, 'r') as f:
-    contents = f.read()
+    contents = f"do(){f.read()}"  # enable mul instructions
 
 def find_entries(text):
     pattern = r'mul\((\d+),(\d+)\)'
     matches = re.findall(pattern, text)
     return matches
 
+# Part 1
 res = find_entries(contents)
 
-# Part 1
-sum = 0
 for r in res:
-    sum += int(r[0])*int(r[1])
+    sum1 += int(r[0]) * int(r[1])
 
-print(sum)
+# Part 2
+do = r'\bdo\(\)'; dont = r'\bdon\'t\(\)'
+sections = re.split(f'({do}|{dont})', contents)
+
+for section in sections:
+    section = section.strip()
+    
+    # if re.fullmatch(do, section):
+    #     state = True
+    # elif re.fullmatch(dont, section):
+    #     state = False
+    # elif state:
+    #     res = find_entries(section)
+    #     for r in res:
+    #         sum2 += int(r[0]) * int(r[1])
+
+    state = re.fullmatch(do, section) is not None or (re.fullmatch(dont, section) is None and state)
+    if state:
+        res = find_entries(section)
+        for r in res:
+            sum2 += int(r[0]) * int(r[1])
+
+print(f"{sum1}\n{sum2}")
