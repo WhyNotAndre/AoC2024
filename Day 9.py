2e5278824b985fa98d5ce4e2986b247e1b12b57b
Andre L. <76952766+WhyNotAndre@users.noreply.github.com>
2024-12-09 15:44:19 +0300

    part 1

---
+++ Day 9.py
@@ -0,0 +1,35 @@
+inp = r'C:\stuff4coding\proj\python\small stuff\AoC 2024\9inp.txt' # path to input file
+
+with open(inp) as f:
+    line = f.read()
+
+lst = []
+
+for i, elem in enumerate(line):
+    if i % 2 == 0:
+        for j in range(int(elem)):
+            lst.append(int(i/2))
+    else:
+        for k in range(int(elem)):
+            lst.append("")
+
+
+stopflg = False
+for i, item in enumerate(reversed(lst)):
+    if item != "":
+        for j, jtem in enumerate(lst):
+            if jtem == "":
+                if j < len(lst)-i-1:
+                    lst[j] = item
+                    lst[len(lst)-i-1] = ""
+                    break
+                else:
+                    stopflg = True
+    if stopflg:
+        break
+
+sum = 0
+for n, num in enumerate(lst):
+    if num != "":
+        sum += n*num
+print(sum)
